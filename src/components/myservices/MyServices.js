dc14e379d75eab0d9904d3db9f9b584dccac3fcd
rupesh401 <gautamrupesh401@gmail.com>
2023-03-11 11:42:58 -0500

    data updated

--- src/components/myservices/MyServices.js
+++ src/components/myservices/MyServices.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCode, faCrop, faHandScissors } from "@fortawesome/fontawesome-free-solid";
+import {
+  faCode,
+  faCrop,
+  faHandScissors,
+} from "@fortawesome/fontawesome-free-solid";
 import "../myservices/MyServices.css";
 
 const MyServices = () => {
@@ -9,10 +13,7 @@ const MyServices = () => {
       <h1 className="myservices">My Services</h1>
       <div className="services">
         <div className="service_list">
-          <FontAwesomeIcon
-            className="font_icon fa-5x"
-            icon={faCode}
-          />
+          <FontAwesomeIcon className="font_icon fa-5x" icon={faCode} />
           <p className="service_list_name">Web Design</p>
           <p className="service_list_desc">
             I help users to design their site in professional way with excellent
@@ -29,10 +30,10 @@ const MyServices = () => {
         </div>
         <div className="service_list">
           <FontAwesomeIcon className="font_icon fa-5x" icon={faHandScissors} />
-          <p className="service_list_name">Web Design</p>
+          <p className="service_list_name">Frontend Design (React) </p>
           <p className="service_list_desc">
-            I help users to design their site in professional way with excellent
-            look and feel.
+            I help users to design their site in professional with React state
+            and redux global state management.
           </p>
         </div>
       </div>
