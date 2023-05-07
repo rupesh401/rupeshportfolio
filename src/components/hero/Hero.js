d7ffd0bb19e66cc72b44a5f49ca21d5dc0b05a52
Rupesh Gautam <43663818+rupesh401@users.noreply.github.com>
2023-05-07 01:57:05 -0400

    update resume
    no resume file

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -52,7 +52,8 @@ const Hero = () => {
                 <FontAwesomeIcon icon={faAddressBook} className="px-3" />
                 Contact Me
               </button>
-              <a href="images/Resume_new.pdf" className="btn btn_download">
+//images/Resume_new.pdf
+              <a href="" className="btn btn_download">
                 Download Resume
               </a>
             </div>
